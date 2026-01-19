dac73f95a97e340bd3e50e02b86a9cc5d8bbcd64
tarun <tarunjangra1903@gmail.com>
2026-01-19 18:59:38 +0530

    Initialized the project and set up the basic folder structure

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      < div className="min-h-screen flex items-center justify-center bg-gray-100">
+        <h1 className="text-4xl font-bold text-blue-600">Hello, Vite + React + TailwindCSS!</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
